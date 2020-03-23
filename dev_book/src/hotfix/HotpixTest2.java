9955cc1461ab5320b2e7ebacf464505e16888961
komso_09 <ehwnshd2@naver.com>
2020-03-23 15:56:30 +0900

    hotpix2

---
+++ dev_book/src/hotfix/HotpixTest2.java
@@ -0,0 +1,11 @@
+package hotfix;
+
+public class HotpixTest2 {
+
+	public static void main(String[] args) {
+		//hotfix브랜치에서 저장점을 한개 더 가지기 위한 소스 추가 
+		System.out.println("hotfix 브랜츠에서 2번째로 추가한 소스임.");
+
+	}
+
+}
